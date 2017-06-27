48ad835363555574fb77cbdce8a7f77b5817f06b
Darren <dk.darrenkeen@gmail.com>
2017-06-27 23:26:03 +0100

    Finished best hole scorecard

--- client/app/common/getScores/getScores.service.js
+++ client/app/common/getScores/getScores.service.js
@@ -4,9 +4,40 @@ export default function (CoreConstants) {
   return {
 
     players: CoreConstants.players,
+    scores: CoreConstants.scores,
 
     getBestHoleScores(course) {
-      const scorecard = [];
+
+      const courseScores = this.scores.filter((score) => score.course_id === course);
+      let scorecard = [];
+      for (let i = 0; i < 18; i++) {
+        scorecard.push({
+          "player_id": [],
+          "score": 11
+        });
+      }
+
+      if (courseScores.length > 0) {
+        courseScores.map((scoreEntry) => {
+          let player_id = scoreEntry.player_id;
+          scoreEntry.score.map((score, index) => {
+
+            let thisScorecardHole = scorecard[index].score;
+
+            if (score === thisScorecardHole) {
+
+              if (scorecard[index].player_id.indexOf(player_id) < 0) {
+                scorecard[index].player_id.push(player_id);
+              }
+
+            } else if (score < thisScorecardHole) {
+              scorecard[index].score = score;
+              scorecard[index].player_id = [player_id];
+            }
+
+          });
+        })
+      }
 
       return scorecard;
     },
--- client/app/common/scorecard/scorecard.component.js
+++ client/app/common/scorecard/scorecard.component.js
@@ -1,5 +1,6 @@
 import template from './scorecard.html';
 import './scorecard.scss';
+import 'lodash';
 import * as firebase from 'firebase';
 
 class ScorecardController {
@@ -30,6 +31,9 @@ class ScorecardController {
     if(this.isBestHoleCard()) {
       this.courseInformation = this.GetScoresService.getCourseInformation(this.selectedCourse);
       this.scores = this.GetScoresService.getBestHoleScores(this.selectedCourse);
+      this.courseInformation.course_holes.map((hole, index) => {
+        Object.assign(hole, this.scores[index]);
+      });
     }
 
     if(this.isBestRoundCard()) {
@@ -55,6 +59,24 @@ class ScorecardController {
     if(this.isBestHoleCard()) return 'isBestHoleCard';
     if(this.isBestRoundCard()) return 'isBestRoundCard';
   }
+
+  getPlayerNames(players) {
+    let playerNames = "";
+    if(players.length > 1) {
+      for(let player of players) {
+        if(players[players.length - 1] === player) {
+          playerNames += ` & ${player}`;
+        } else if(player !== players[0]) {
+          playerNames += `, ${player}`;
+        } else {
+          playerNames = player;
+        }
+      }
+    } else {
+      playerNames = players[0];
+    }
+    return playerNames;
+  };
 
   getBestHoleScore(hole) {
     //TODO: calculate best hole
